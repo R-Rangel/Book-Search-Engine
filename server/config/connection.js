46a3d5fab2d7d1135638ccf1f3b2d97e832dd958
R-Rangel <r.rangel.0222@gmail.com>
2023-04-04 00:21:53 -0500

    updated type defs, resolvers, indexjs

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.set('strictQuery', false);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/googlebooks');
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,13 +11,31 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Create the Apollo Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => {
+    // Apply the authMiddleware to the request object and pass it to the context
+    const authenticatedReq = authMiddleware({ req });
+    return { user: authenticatedReq.user };
+  },
+});
+
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// Start the server after the Apollo Server has been applied
+db.once('open', async () => {
+  // Add this line
+  await server.start();
+  // And apply the middleware
+  server.applyMiddleware({ app });
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`🚀 GraphQL Server ready at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
